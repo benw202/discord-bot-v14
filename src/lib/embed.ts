052d728dbc00b61a4ede47437aa322448d5f3d0e
Ben Williams <bwilliams12@me.com>
2022-07-19 21:45:39 +0100

    New EmbedBuilder

--- src/lib/embed.ts
+++ src/lib/embed.ts
@@ -1,8 +1,15 @@
 import discord from 'discord.js';
 import { Colors } from '../constants';
 
-export function makeEmbed(embed: discord.MessageEmbedOptions): discord.MessageEmbed {
-    return new discord.MessageEmbed({
+//export function makeEmbed(embed: discord.EmbedAuthorOptions): discord.EmbedBuilder {
+//    return new discord.EmbedBuilder({
+//        color: Colors.FBW_CYAN,
+//        ...embed,
+//    });
+//}
+
+export function makeEmbed(embed): discord.EmbedBuilder {
+    return new discord.EmbedBuilder({
         color: Colors.FBW_CYAN,
         ...embed,
     });
